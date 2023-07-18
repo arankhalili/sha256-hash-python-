406bebe4719493ae0556c7241a30c84f4e7a781b
Aran <100995821+arankhalili@users.noreply.github.com>
2023-07-18 14:59:00 +0330

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,10 +1,15 @@
 import hashlib
 
+#This program is a sha256 hash that when you add a 4-digit number to it,it gives you a 16-digit phrase that has encrypted your number.
+
 def hashing(number):
     number_str = str(number).encode('utf-8')
     hash = hashlib.sha256(number_str)
     hash_hex = hash.hexdigest()
     return hash_hex
+#In this part, it asks you for a 4-digit password:
 
 hash1234 = hashing(input('give me a 4-digit number!'))
 print(hash1234)
+
+#you can copy this code and paste it in your python IDE to use it.
